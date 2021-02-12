638c82dae6d0aefe5b97b559efddeb8680086292
whirlybirb <hellion1024@gmail.com>
2021-02-11 21:38:35 -0500

    Refactored perches (again)
    Now point to point only handles movement and binoculars finds perches based on the perches tag and can turn on and off highliting directly with a generic function

--- Assets/Scripts/Binoculars.cs
+++ Assets/Scripts/Binoculars.cs
@@ -17,6 +17,8 @@ public class Binoculars : MonoBehaviour
 
     private Camera cam;
 
+    private Transform currentPerch;
+
 
     // Start is called before the first frame update
     void Start()
@@ -42,6 +44,10 @@ public class Binoculars : MonoBehaviour
         if (Input.GetMouseButtonUp(1))
         {
             weZoomin = false;
+            if (currentPerch) //If you have a perch selected, de-highlite it
+            {
+                setOutline(false, currentPerch);
+            }
         }
         if (!weZoomin)
         {
@@ -60,27 +66,38 @@ public class Binoculars : MonoBehaviour
         //see what ray hits
         if (Physics.Raycast(camRay, out hit)) //if the raycaster collides with anything
         {
-            if(GetComponent<PointToPoint>().checkPerches(hit.transform)) //check if that anything is a perch
+            if(hit.transform.gameObject.tag == "perch") //check if that anything is a perch
             {
-                GetComponent<PointToPoint>().setOutline(true); //turn on the highliting of anyperch it is colliding with
+                currentPerch = hit.transform;
+                setOutline(true, currentPerch); //turn on the highliting of anyperch it is colliding with
                 if (Input.GetMouseButtonUp(0)) //go to that perch if you click rmb
                 {
-                    GetComponent<PointToPoint>().getMoving();
+                    GetComponent<PointToPoint>().getMoving(currentPerch.position);
                 }
             }
-            else //if it's not, turn anything highlighted off
+            else if(currentPerch) //if it's not, turn anything highlighted off
             {
-                GetComponent<PointToPoint>().setOutline(false);
+                setOutline(false, currentPerch);
             }
         }
-        else  //If the raycaster collides with absolutly nothing, turn anything highlighted off
+        else if (currentPerch) //If the raycaster collides with absolutly nothing, turn anything highlighted off
         {
-            GetComponent<PointToPoint>().setOutline(false);
+            setOutline(false, currentPerch);
         }
     }
 
     public bool getZoom()
     {
         return weZoomin;
+    }
+
+    public void setOutline(bool set, Transform obj)
+    {
+        //tell you if you are currently colliding with the current selected perch
+        //if (currentPerch && currentPerch.gameObject.GetComponent<Outline>()) Debug.Log("currentPerch "+currentPerch.name+": "+ currentPerch.gameObject.GetComponent<Outline>().enabled);
+        //else Debug.Log("currentPerch: null");
+
+        //if current perch is not null and the current selected perch has the outline script, set it to whatever set is
+        if (obj.GetComponent<Outline>()) obj.GetComponent<Outline>().enabled = set;
     }
 }
--- Assets/Scripts/PointToPoint.cs
+++ Assets/Scripts/PointToPoint.cs
@@ -6,64 +6,29 @@ using UnityStandardAssets.Characters.ThirdPerson;
 
 public class PointToPoint : MonoBehaviour
 {
-    public GameObject Points; //all the possible perches
-    private Camera cam;
-    public List<Transform> perches; //the transforms of said perches
-    public Transform currentPerch; //the perch you are currently looking at
-
-    //old an new positions for moving
-    private Vector3 oldPos;
+    //new position for moving
     private Vector3 newPos;
 
     private bool moving; //are you moving
     public float smooth; //smooth the movement
     public float minDist; //minimum distance to stop moving between you and your destination (so you don't get stuck in zenos paradox)
+
     // Start is called before the first frame update
     void Start()
     {
-        cam = GetComponentInChildren<Camera>();
         moving = false;
-        foreach (Transform point in Points.GetComponentsInChildren<Transform>())
-        {
-            perches.Add(point);
-        }        
     }
 
-    public bool checkPerches(Transform hit)
-    {
-        foreach (Transform perch in perches)
-        {
-            if (hit == perch)
-            {
-                //Debug.Log("hitting perch: " + perch.name);
-                //set whatever perch you are looking at to be the current perch
-                currentPerch = perch;
-                //then say that yes you are in fact looking at a viable perch
-                return true;
-            }
-        }
-        return false;
-    }
 
-    public void getMoving()
+    public void getMoving(Vector3 newps)
     {
-
-        oldPos = transform.position;
-        newPos = currentPerch.position;
+        newPos = newps;
         moving = true;
 
         //turn off the controller so the flight looks a little smoother
         GetComponent<FirstPersonController>().enabled = false;
     }
 
-    public void setOutline(bool set)
-    {
-        //tell you if you are currently colliding with the current selected perch
-        //if (currentPerch && currentPerch.gameObject.GetComponent<Outline>()) Debug.Log("currentPerch "+currentPerch.name+": "+ currentPerch.gameObject.GetComponent<Outline>().enabled);
-        //else Debug.Log("currentPerch: null");
-        //if current perch is not null and the current selected perch has the outline script, set it to whatever set is
-        if (currentPerch && currentPerch.gameObject.GetComponent<Outline>()) currentPerch.gameObject.GetComponent<Outline>().enabled = set;
-    }
 
     // Update is called once per frame
     void Update()
@@ -81,10 +46,8 @@ public class PointToPoint : MonoBehaviour
             //when you are close enough (as determined be minDist) to the new position, turn control back on and stop moving
             if (Vector3.Distance(transform.position, newPos) <= minDist)
             {
-                if (currentPerch.gameObject.GetComponent<Outline>()) currentPerch.gameObject.GetComponent<Outline>().enabled = false;
                 GetComponent<FirstPersonController>().enabled = true;
                 moving = false;
-                currentPerch = null;
             }
         }
     }
